4590f937b3e844bb26e2a25eb158a6a95174ecd7
Ilkka Tengvall <itengval@redhat.com>
2024-03-11 21:30:44 +0200

    Add python and yaml formatters

--- init.lua
+++ init.lua
@@ -541,6 +541,9 @@ require('lazy').setup({
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'black' },
+        yaml = { 'yamlfix' },
+        markdown = { 'markdownlint' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
