b6009c7fadbafa09d43f994dd7868cc2e86ca8f7
Ilkka Tengvall <itengval@redhat.com>
2024-03-14 16:30:56 +0200

    Try enabling yaml LSP for kube

--- init.lua
+++ init.lua
@@ -784,5 +784,93 @@ require('lazy').setup({
 --   },
 -- }
 
+local cfg = require('yaml-companion').setup {
+  -- Tried to follow the following guides:
+  -- https://neovimcraft.com/plugin/someone-stole-my-name/yaml-companion.nvim
+  -- https://www.arthurkoziel.com/json-schemas-in-neovim/
+  -- https://www.redhat.com/en/blog/validating-openshift-manifests-in-a-gitops-world
+
+  -- detect k8s schemas based on file content
+  builtin_matchers = {
+    kubernetes = { enabled = true },
+  },
+
+  -- schemas available in Telescope picker
+  schemas = {
+    -- not loaded automatically, manually select with
+    -- :Telescope yaml_schema
+    {
+      name = 'Argo CD Application',
+      uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json',
+    },
+    {
+      name = 'SealedSecret',
+      uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/bitnami.com/sealedsecret_v1alpha1.json',
+    },
+    -- schemas below are automatically loaded, but added
+    -- them here so that they show up in the statusline
+    {
+      name = 'Kustomization',
+      uri = 'https://json.schemastore.org/kustomization.json',
+    },
+    {
+      name = 'GitHub Workflow',
+      uri = 'https://json.schemastore.org/github-workflow.json',
+    },
+    {
+      -- name = 'Kubernetes 1.22.4',
+      -- uri = 'https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json',
+      name = 'Kubernetes 1.26.9',
+      uri = 'https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.26.9-standalone-strict/all.json',
+    },
+    {
+      -- name = 'Kubernetes 1.22.4',
+      -- uri = 'https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.4-standalone-strict/all.json',
+      name = 'OpanShift 4.14',
+      uri = 'file:///var/home/itengval/src/ocp-json-schema/k8s-manifest-validation/schemas/openshift-json-schema/master-standalone',
+    },
+  },
+
+  lspconfig = {
+    settings = {
+      yaml = {
+        validate = true,
+        schemaStore = {
+          enable = false,
+          url = '',
+        },
+
+        -- schemas from store, matched by filename
+        -- loaded automatically
+        -- schemas = require('schemastore').yaml.schemas {
+        --   select = {
+        --     'kustomization.yaml',
+        --     'GitHub Workflow',
+        --   },
+        -- },
+      },
+    },
+  },
+}
+
+require('lspconfig')['yamlls'].setup(cfg)
+
+require('telescope').load_extension 'yaml_schema'
+
+-- get schema for current buffer
+local function get_schema()
+  local schema = require('yaml-companion').get_buf_schema(0)
+  if schema.result[1].name == 'none' then
+    return ''
+  end
+  return schema.result[1].name
+end
+
+require('mini.statusline').setup {
+  sections = {
+    statusline = { 'fileformat', 'filetype', get_schema },
+  },
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
